655642ae23317afeba7256a135ac9970b56694d1
miokasa <wdn200211@gmail.com>
2024-12-22 21:46:34 -0600

    data source

---
+++ data_source.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+
+sp500 = yf.download('^GSPC', start="2014-11-01", end="2024-11-1")
+sp500.to_csv('sp500_data.csv')
+
+
+eth_data = yf.download("ETH-USD", start="2014-11-01", end="2024-11-1")
+eth_data.to_csv("ETH_etf.csv")
+
+
+btc_data = yf.download("BTC-USD", start="2014-11-01", end="2024-11-1")
+btc_data.to_csv("bitcoin_historical_data.csv")
